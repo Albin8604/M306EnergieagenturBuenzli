a62dddba9f14d4d59fcebe26f8ee082054dfd2da
Albin8604 <smrqakua@bzz.ch>
2023-05-16 11:16:25 +0200

    added main ui and controller, adapted css file

---
+++ src/main/java/ch/albin/energieagentur/controller/MainController.java
@@ -0,0 +1,75 @@
+package ch.albin.energieagentur.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+public class MainController extends Controller{
+    public ComboBox valuesComboBox;
+    public DatePicker fromDatePicker;
+    public DatePicker toDatePicker;
+    public LineChart dataChart;
+
+    @Override
+    public void init() {
+
+    }
+
+    public void selectSDATFolder(ActionEvent actionEvent) {
+
+    }
+
+    public void selectESLFolder(ActionEvent actionEvent) {
+
+    }
+
+    public void fromMonthBackward(ActionEvent actionEvent) {
+
+    }
+
+    public void fromDayBackward(ActionEvent actionEvent) {
+
+    }
+
+    public void fromDayForward(ActionEvent actionEvent) {
+
+    }
+
+    public void fromMonthForward(ActionEvent actionEvent) {
+
+    }
+
+    public void toMonthBackward(ActionEvent actionEvent) {
+
+    }
+
+    public void toDayBackward(ActionEvent actionEvent) {
+
+    }
+
+    public void toDayForward(ActionEvent actionEvent) {
+
+    }
+
+    public void toMonthForward(ActionEvent actionEvent) {
+
+    }
+
+    public void dayForward(ActionEvent actionEvent) {
+
+    }
+
+    public void dayBackward(ActionEvent actionEvent) {
+
+    }
+
+    public void monthForward(ActionEvent actionEvent) {
+
+    }
+
+    public void monthBackward(ActionEvent actionEvent) {
+
+    }
+}
