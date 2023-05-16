5a37e114390f7298dc0decd8b7339b5c0326fe69
JoroKudo <jiro.fessler@gmail.com>
2023-05-16 11:53:26 +0200

    added comments

--- src/main/java/ch/albin/energieagentur/sdatParseTest/SdatParser.java
+++ src/main/java/ch/albin/energieagentur/sdatParseTest/SdatParser.java
@@ -14,35 +14,62 @@ import org.xml.sax.SAXException;
 public class SdatParser {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException {
+        // Path to the SDAT XML file
         String filePath = "src/main/resources/SDAT-Files/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml";
         try {
+            // Create a File object with the specified file path
             File file = new File(filePath);
+
+            // Create a DocumentBuilderFactory
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+            // Create a DocumentBuilder
             DocumentBuilder db = dbf.newDocumentBuilder();
+
+            // Parse the XML file and obtain the Document object
             Document document = db.parse(file);
+
+            // Normalize the document to ensure consistent handling of XML data
             document.getDocumentElement().normalize();
+
+            // Print the root element name
             System.out.println("Root Element: " + document.getDocumentElement().getNodeName());
 
+            // Retrieve a list of ValidatedMeteredData_HeaderInformation elements
             NodeList infoList = document.getElementsByTagName("rsm:ValidatedMeteredData_HeaderInformation");
+
+            // Retrieve a list of MeteringData elements
             NodeList dataList = document.getElementsByTagName("rsm:MeteringData");
 
             System.out.println("----------------------------");
 
+            // Get the first MeteringData element
             Element dataElement = (Element) dataList.item(0);
+
+            // Get the first ValidatedMeteredData_HeaderInformation element
             Element infoElement = (Element) infoList.item(0);
 
+            // Retrieve the Sender element within the HeaderInformation
             Element sElement = (Element) infoElement.getElementsByTagName("rsm:Sender").item(0);
             System.out.println("senderID: " + sElement.getElementsByTagName("rsm:ID").item(0).getTextContent());
+
+            // Retrieve the Receiver element within the HeaderInformation
             Element rElement = (Element) infoElement.getElementsByTagName("rsm:Receiver").item(0);
             System.out.println("receiverID: " + rElement.getElementsByTagName("rsm:ID").item(0).getTextContent());
 
+            // Retrieve a list of Observation elements within the MeteringData
             NodeList oList = dataElement.getElementsByTagName("rsm:Observation");
+
+            // Get the unit of measurement for the data
             String unit = dataElement.getElementsByTagName("rsm:MeasureUnit").item(0).getTextContent();
+
+            // Iterate over the Observation elements and print their details
             for (int i = 0; i < oList.getLength(); i++) {
                 Element oElement = (Element) oList.item(i);
                 System.out.println("Observation: seq: " + oElement.getElementsByTagName("rsm:Sequence").item(0).getTextContent() + ", Volume: " + oElement.getElementsByTagName("rsm:Volume").item(0).getTextContent() + unit);
             }
         } catch (IOException e) {
+            // Handle IOException by printing the error message
             System.out.println(e);
         }
     }
